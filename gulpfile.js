e79990080d44c51637fe5669c3fa1906c617205f
Vitaliy <me@harchenko.pro>
2017-06-03 21:26:23 +0500

    frontend stable

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ const gulp = require('gulp');
 const scss = require('gulp-scss');  // https://github.com/jxnblk/css-scss
 const concat = require('gulp-concat');
 const debug = require('gulp-debug');
-const sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require('gulp-sourcemaps'); // нужны для разработки, помогают в дебаггинге
+const gulpIf = require('gulp-if');
+const del = require('del');
 
 // HOW TO CREATE TASK
 gulp.task('example:callback', function(callback) {
@@ -27,15 +29,29 @@ gulp.task('example:dest', function() {
 			console.log(file);
 		})
 		.pipe(gulp.dest('dest'));
-})
+});
 
 // Начальная сборка
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+// NODE_ENV=production gulp styles
+
 gulp.task('styles', function() {
 	return gulp.src('frontend/styles/main.scss', {base: 'frontend'})
-		.pipe(sourcemaps.init())
+		.pipe(gulpIf(isDevelopment, sourcemaps.init()))
 		.pipe(scss())
-		.pipe(sourcemaps.write())
+		.pipe(gulpIf(isDevelopment, sourcemaps.write()))
 		.pipe(debug({title: 'scss'}))
 		.pipe(concat('all.css'))
 		.pipe(gulp.dest('dest'));
-})
+});
+
+gulp.task('assets', function() {
+	return gulp.src('frontend/assets/**')
+		.pipe(gulp.dest('dest'));
+});
+
+gulp.task('clean', function() {
+	return del('dest');
+});
+
+gulp.task('build', gulp.series('clean', 'styles', 'assets'));
