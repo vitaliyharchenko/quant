01c081458a22555ee4688fa0f0e7607622190865
Vitaliy Harchenko <harchenko.grape@gmail.com>
2017-06-18 14:54:55 +0500

    Django staticfiles refactoring & comments

--- django/conf/base/assets.py
+++ django/conf/base/assets.py
@@ -6,8 +6,24 @@ import os
 
 STATIC_URL = '/static/'
 
+# Сюда упадут все файлы на production после collectstatic
+# Важно не использовать STATICFILES_FINDERS на production, а например nginx 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
+# В этих директориях django ищет статические файлы.
+# Если не находит, то ищет с помощью django.contrib.staticfiles.finders.AppDirectoriesFinder,
+# которая проверяет папку static каждого установленного в проекте приложения
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets/dist'),
 )
+
+# В режиме разработки — python manage.py runserver — Django ищет статичные файлы с помощью них
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+# пользователи должны лишь единожды скачивать ресурсы!
+# хеширование включается только в режиме DEBUG=false
+# Вам понадобится перезапускать свой проект, используя python manage.py startapp, при каждом изменении шаблона.
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
