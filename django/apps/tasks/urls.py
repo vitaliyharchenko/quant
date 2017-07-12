58d565ac2867e2f04b8df25ea551e4cb19df375b
ekaterinaparamonova <teleport19@gmail.com>
2017-07-12 17:00:24 +0500

    Task list view changes

--- django/apps/tasks/urls.py
+++ django/apps/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
+from .views import TaskList
 
 urlpatterns = [
-    url(r'^$', views.task_list, name="main"),
+    url(r'^$', TaskList.as_view(), name="main"),
     url(r'^(?P<pk>[0-9]+)$', views.task_detail, name="task"),
 ]
--- django/apps/tasks/views.py
+++ django/apps/tasks/views.py
@@ -2,26 +2,33 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework import generics 
 from .models import Task
 from .serializers import TaskSerializer
 from apps.lessons.models import LessonNodeRelation
 from apps.blocks.models import NodeBlockRelation
 from apps.blocks.serializers import BlockSerializer
 
-@csrf_exempt
-def task_list(request):
-    if request.method == 'GET':
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
-        return JsonResponse(serializer.data, safe=False)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TaskSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+class TaskList(generics.ListAPIView):
+    renderer_classes = (JSONRenderer,)
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# @csrf_exempt
+# def task_list(request):
+#     if request.method == 'GET':
+#         tasks = Task.objects.all()
+#         serializer = TaskSerializer(tasks, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     elif request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = TaskSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)
 
 @csrf_exempt
 def task_detail(request, pk):
