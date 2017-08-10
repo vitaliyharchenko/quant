f6e59f3ccfef71baa9e06085ae69124fba7d6f73
Vitaliy Harchenko <me@harchenko.pro>
2017-08-10 17:23:56 +0500

    Multiple landings

--- django/apps/landings/views.py
+++ django/apps/landings/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def example(request):
 	return render(request, 'landing.html')
+
+
+def landing(request, class_num, subject):
+	tpl_path = 'landings/{}/{}.html'.format(class_num, subject)
+	return render(request, tpl_path)
