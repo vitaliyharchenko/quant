b1c0cd21a2641cfe4c3e2046fe225783050f57d1
ekaterinaparamonova <teleport19@gmail.com>
2017-08-22 18:46:30 +0500

    API changes. Add block detail view

--- django/apps/api/urls.py
+++ django/apps/api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from apps.tasks.views import TaskListViewSet, TaskDetailView, TaskResultView
 from apps.groups.views import GroupDetailView, GroupLessonResultView
+from apps.blocks.views import BlockDetailView
 
 urlpatterns = [
     url(r'^tasks/$', TaskListViewSet.as_view(), name="task_set"),
     url(r'^tasks/(?P<pk>[0-9]+)/$', TaskDetailView.as_view(), name="task_detail"),
     url(r'^tasks/result/(?P<pk>[0-9]+)/$', TaskResultView.as_view(), name="task_result"),
     url(r'^groups/(?P<pk>[0-9]+)/$', GroupDetailView.as_view(), name="group_detail"),
-    url(r'^groups/(?P<group>[0-9]+)/result/(?P<lesson>[0-9]+)/$', GroupLessonResultView.as_view(), name="group_lesson_result")
+    url(r'^groups/(?P<group>[0-9]+)/result/(?P<lesson>[0-9]+)/$', GroupLessonResultView.as_view(), name="group_lesson_result"),
+    url(r'^blocks/(?P<pk>[0-9]+)/$', BlockDetailView.as_view(), name="block_detail")
 ]
--- django/apps/blocks/views.py
+++ django/apps/blocks/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from apps.api.views import AuthMixin
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from .models import Block
+from .serializers import BlockSerializer
 
-# Create your views here.
-def example(request):
-    return render(request, 'index.html')
 
-
-def styleguide(request):
-    return render(request, 'styleguide.html')
+class BlockDetailView(AuthMixin, APIView):
+    def get(self, request, pk, format=None):
+        try:
+            block = Block.objects.get(pk=pk)
+        except Exception as e:
+            return JsonResponse({'Error message': str(e)}, status=404)
+        serializer = BlockSerializer(block)
+        return JsonResponse(serializer.data, status=200)
