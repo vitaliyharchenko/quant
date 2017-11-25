bd1bdc1748cb83fdaaab28fc8310e4c6581f992a
Vitaliy Harchenko <me@harchenko.pro>
2017-11-25 17:19:27 +0500

    Vue fix

--- frontend/vue/src/components/App.vue
+++ frontend/vue/src/components/App.vue
@@ -5,7 +5,7 @@
           <b-navbar-brand href="/app/">Quant</b-navbar-brand>
           <b-collapse is-nav id="nav_collapse">
             <b-nav is-nav-bar>
-              <b-nav-item href="/app/tasks" disabled>Задания</b-nav-item>
+              <b-nav-item href="/app/tasks">Задания</b-nav-item>
             </b-nav>
             <!-- Right aligned nav items -->
             <b-nav is-nav-bar class="ml-auto">
